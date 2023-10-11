927ff3c9a80bcb6d2cac7cc1ad844c3d2131e134
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-10-11 18:48:59 +0900

    lohiyani express qismini shakllantirib olish

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require ('http');
+const mongodb = require ("mongodb")
+
+let db;
+const connectionString = "mongodb+srv://Jasur:ropdFQ6AuB3TmckJ@cluster0.tgyh4h9.mongodb.net/Reja?retryWrites=true&w=majority"
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succed");
+        module.exports = client;
+        const app = require("./app")
+        const server = http.createServer(app);
+        let PORT = 5001;
+        server.listen(PORT, function () {
+         console.log(`The server is running succesful om PORT ${PORT}, http://localhost:${PORT}`);
+    })  
+    }
+});
+
+//Jasur
