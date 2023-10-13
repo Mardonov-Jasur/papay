b3f2af5d7c4bb12ec36bb3290f8fc93e87eaafae
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-10-13 23:51:48 +0900

    fix: modify member schema model part 2

--- server.js
+++ server.js
@@ -7,36 +7,37 @@ const mongoose = require ("mongoose")
 const connectionString = process.env.MONGO_URL;
 
 
-mongoose
- .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log ("Mongoose Connected Successfully");
-        const app = require("./app")
-        const server = http.createServer(app);
-        let PORT = process.env.PORT || 5001;
-        server.listen(PORT, function () {
-         console.log(`The server is running succesful om PORT ${PORT}, http://localhost:${PORT}`);
-    });
-})
-.catch((err) => {
-    console.log("ERROR on connection MongoDB");
-});
-// mongoose.connect(connectionString, {
+// mongoose
+//  .connect(connectionString, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
-// }, (err, goose) => {
-//     if(err) console.log("ERROR on connection MongoDB");
-//     else {
-//         console.log("MongoDB connection succed");
-//         console.log(mongoose);
+// })
+// .then(() => {
+//     console.log ("Mongoose Connected Successfully");
 //         const app = require("./app")
 //         const server = http.createServer(app);
 //         let PORT = process.env.PORT || 5001;
 //         server.listen(PORT, function () {
 //          console.log(`The server is running succesful om PORT ${PORT}, http://localhost:${PORT}`);
-//     })  
-//     }
+//     });
+// })
+// .catch((err) => {
+//     console.log("ERROR on connection MongoDB");
 // });
+mongoose.connect(
+    connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, goose) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succed");
+        // console.log(goose);
+        const app = require("./app")
+        const server = http.createServer(app);
+        let PORT = process.env.PORT || 5001;
+        server.listen(PORT, function () {
+         console.log(`The server is running succesful om PORT ${PORT}, http://localhost:${PORT}`);
+    })  
+    }
+});
