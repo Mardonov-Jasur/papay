091a27502d1c4711d39b4cbaca63bbac0c77f428
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-10-13 22:11:38 +0900

    feat: memeber controllerlarni qurish

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,17 +1,21 @@
 let memberController = module.exports;
 
 memberController.home = (req, res) => {
+    console.log("GET cont.home");
     res.send("Siz home sahifasidasiz");
 };
 
 memberController.signup = (req, res) => {
+    console.log("POST cont.signup");
     res.send("Siz signup sahifasidasiz");
 };
 
 memberController.login = (req, res) => {
+    console.log("POST cont.login");
     res.send("Siz login sahifasidasiz");
 };
 
 memberController.logout = (req, res) => {
+    console.log("GET cont.logout");
     res.send("Siz logout sahifasidasiz");
 };
