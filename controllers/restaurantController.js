68fb1e9e5e379683e694e56b285bffa104dfbafc
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-10-19 18:15:26 +0900

    feat: develop session authentication on ejs

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,6 +2,18 @@ const Member = require("../models/Member");
 
 let restaurantController = module.exports;
 
+restaurantController.getMyRestaurantData = async (req, res) => {
+    try {
+        console.log ("GET: cont/getMyRestaurantData");
+        // TODO: Get my restaurant products
+
+        res.render("restaurant-menu");
+    } catch(err) {
+        console.log (`ERROR, cont/getMyRestaurantData, ${err.message}`);
+        res.json({state: "fail", message: err.message});
+    }
+}
+
 restaurantController.getSignupMyRestaurant = async (req, res) => {
     try {
         console.log ("GET: cont/getSignupMyRestaurant");
@@ -19,9 +31,10 @@ restaurantController.signupProcess = async (req, res) => {
           member = new Member(),
           new_member = await member.signupDate(data);
 
-          //SESSION
+          req.session.member = new_member;
+          res.redirect("/resto/products/menu");
 
-        res.json({state: "succed", data: new_member});
+        
     } catch (err) {
         console.log (`ERROR, cont/signup, ${err.message}`);
         res.json({state: "fail", message: err.message});
@@ -45,7 +58,11 @@ restaurantController.loginProcess = async (req, res) => {
           member = new Member(),
           result = await member.loginDate(data);
 
-        res.json({state: "succed", data: result});
+        req.session.member = result;
+        req.session.save(function () {
+            res.redirect("/resto/products/menu");
+        });  
+
     } catch (err) {
         console.log (`ERROR, cont/login, ${err.message}`);
         res.json({state: "fail", message: err.message});
@@ -55,4 +72,12 @@ restaurantController.loginProcess = async (req, res) => {
 restaurantController.logout = (req, res) => {
     console.log("GET cont.logout");
     res.send("Siz logout sahifasidasiz");
+};
+
+restaurantController.checkSessions = (req, res) => {
+    if (req.session.member) {
+       res.json({state: "succed", data: req.session.member });
+    } else {
+        res.json({state: "fail", message: "You are not authenticated"}); 
+    }
 };
