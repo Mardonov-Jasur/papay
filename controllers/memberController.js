13cb22297d09321e3960e80c2b230d8f7901e690
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-12-01 15:51:00 +0900

    faet: create jwt tokens and enable cookie-parser

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,38 +1,71 @@
+const assert = require("assert");
 const Member = require("../models/Member");
 
 let memberController = module.exports;
+const jwt = require("jsonwebtoken");
+const Definer = require("../lib/mistake");
 
 memberController.signup = async (req, res) => {
-    try {
-        console.log ("POST: cont/signup");
-        const data = req.body,
-          member = new Member(),
-          new_member = await member.signupDate(data);
+  try {
+    console.log("POST: cont/signup");
+    const data = req.body,
+      member = new Member(),
+      new_member = await member.signupDate(data);
 
-          // TODO: AUTHENTICATE BASED ON JWT 
+     const token = memberController.createToken(new_member);
+     res.cookie("access_token", token, {
+       maxAge: 6 * 3600 * 1000,
+       httpOnly: true
+     });
 
-        res.json({state: "succed", data: new_member});
-    } catch (err) {
-        console.log (`ERROR, cont/signup, ${err.message}`);
-        res.json({state: "fail", message: err.message});
-    }
+    res.json({ state: "succed", data: new_member });
+  } catch (err) {
+    console.log(`ERROR, cont/signup, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
 memberController.login = async (req, res) => {
-    try {
-        console.log ("POST: cont/login");
-        const data = req.body,
-          member = new Member(),
-          result = await member.loginDate(data);
+  try {
+    console.log("POST: cont/login");
+    const data = req.body,
+      member = new Member(),
+      result = await member.loginDate(data);
 
-        res.json({state: "succed", data: result});
-    } catch (err) {
-        console.log (`ERROR, cont/login, ${err.message}`);
-        res.json({state: "fail", message: err.message});
-    }
+    const token = memberController.createToken(result);
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true
+    });
+
+    res.json({ state: "succed", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/login, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
 memberController.logout = (req, res) => {
-    console.log("GET cont.logout");
-    res.send("Siz logout sahifasidasiz");
+  console.log("GET cont.logout");
+  res.send("Siz logout sahifasidasiz");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      my_type: result.my_type
+    };
+
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "6h"
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
