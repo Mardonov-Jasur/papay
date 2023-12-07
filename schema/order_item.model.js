a8ba77e33e540fbd48aab361fae823cdc2db16b8
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-12-07 23:38:10 +0900

    feat: develop order creation part one

--- schema/order_item.model.js
+++ schema/order_item.model.js
@@ -7,7 +7,7 @@ const orderItemSchema = new mongoose.Schema(
     item_qauntity: { type: Number, required: true },
     item_price: { type: Number, required: true },
     order_id: { type: Schema.Types.ObjectId, ref: "Order", required: false },
-    [roduc_id]: {
+    product_id: {
       type: Schema.Types.ObjectId,
       required: false,
       ref: "Product",
