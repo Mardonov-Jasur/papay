fba65d354c169c5b4bb713d885d4de8d88d56670
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-12-12 15:46:06 +0900

    [79-80-81] feat: develop getMemberArticles rest api business logic

--- models/Community.js
+++ models/Community.js
@@ -15,7 +15,7 @@ class Community {
       data.mb_id = shapeIntoMongooseObjectId(member._id);
       const new_article = await this.saveArticleData(data);
       return new_article;
-    } catch(err) {
+    } catch (err) {
       throw err;
     }
   }
@@ -27,6 +27,38 @@ class Community {
     } catch (mongo_err) {
       console.log(mongo_err);
       throw new Error(Definer.auth_err1);
+    }
+  }
+
+  async getMemberArticlesData(member, mb_id, inquery) {
+    try {
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
+      mb_id = shapeIntoMongooseObjectId(mb_id);
+      const page = inquery["page"] ? inquery["page"] * 1 : 1;
+      const limit = inquery["limit"] ? inquery["limit"] * 1 : 5;
+
+      const result = await this.boArticleModel
+        .aggregate([
+          { $match: { mb_id: mb_id, art_status: "active" } },
+          { $sort: { createdAt: -1 } },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+          {
+            $lookup: {
+              from: "members",
+              localField: "mb_id",
+              foreignField: "_id",
+              as: "member_data"
+            }
+          },
+          {$unwind: "$member_data"},
+        ])
+        .exec();
+      assert.ok(result, Definer.article_err2);
+
+      return result;
+    } catch (err) {
+      throw err;
     }
   }
 }
